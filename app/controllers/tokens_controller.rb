8f9cc2d14c0ade3c8366c878875a023df383f1db
Kaysha George <kayshageorge@icloud.com>
2018-03-07 19:18:51 -0800

    set up tokens for authorization

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,3 +1,27 @@
 class TokensController < ApplicationController
-  
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user&.authenticate(params[:password])
+      render json: {
+        jwt: encode_token({
+          id: user.id,
+          band_name: user.band_name
+        })
+      }
+    else
+      head :not_found
+    end
+  end
+
+  private
+
+  def encode_token(payload = {}, exp = 24.hours.from_now)
+
+    payload[:exp] = exp.to_i
+    JWT.encode(
+      payload,
+      Rails.application.secrets.secret_key_base
+    )
+  end
 end
