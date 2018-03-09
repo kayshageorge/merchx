d36fea53628ec39462ac1f2996ba8df08db11db6
Kaysha George <kayshageorge@icloud.com>
2018-03-08 17:52:10 -0800

    Fix authorization and user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,6 +47,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:band_name, :email, :password_digest, :description)
+      params.require(:user).permit(:band_name, :email, :password, :password_confirmation, :description)
     end
 end
