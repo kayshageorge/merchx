ea5db10ef94de98bd52ff0b0b4619e417cc72660
Kaysha George <kayshageorge@icloud.com>
2018-03-28 20:43:44 -0700

    tweaks in skus and user controllers

--- app/controllers/skus_controller.rb
+++ app/controllers/skus_controller.rb
@@ -2,7 +2,6 @@ class SkusController < ApplicationController
   before_action :authenticate_user!
   before_action :set_sku, only: [:show, :update, :destroy]
 
-  # GET /skus
   def index
     all_skus = Sku.where(product_id:params[:product_id])
     @skus = all_skus.order(id: :asc)
@@ -17,12 +16,10 @@ class SkusController < ApplicationController
     render json: @sku
   end
 
-  # GET /skus/1
   def show
     render json: @sku
   end
 
-  # POST /skus
   def create
     @sku = Sku.new(sku_params)
 
@@ -33,7 +30,6 @@ class SkusController < ApplicationController
     end
   end
 
-  # PATCH/PUT /skus/1
   def update
     if @sku.update(sku_params)
       render json: @sku
@@ -42,18 +38,16 @@ class SkusController < ApplicationController
     end
   end
 
-  # DELETE /skus/1
   def destroy
     @sku.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_sku
       @sku = Sku.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def sku_params
       params.require(:sku).permit(:product_id, :size, :price, :qty)
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  validates :band_name, :password, presence: true
+  validates :band_name, presence: true
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
